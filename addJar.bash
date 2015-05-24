bca20194a0ba03f9829553cc4c590e8db24b73b6
Dylan Hutchison <dhutchis@mit.edu>
2015-05-23 23:30:44 -0400

    fix up testing scripts

--- addJar.bash
+++ addJar.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e #command fail -> script fail
 set -u #unset variable reference causes script fail
-cp target/d4m_api_java*.jar $ACCUMULO_HOME/lib/ext
-cp target/d4m_api_java*.jar $D4M_HOME/lib
-unzip -ouq target/libext*.zip -d $D4M_HOME
+
+# Install server-side iterators in Accumulo.
+cp graphulo/target/graphulo-*.jar $ACCUMULO_HOME/lib/ext
+
+# Install client code + dependencies in D4M.
+cp graphulo/target/graphulo-*.jar $D4M_HOME/lib
+unzip -ouq graphulo/target/libext*.zip -d $D4M_HOME
+# Replace DBinit.m in D4M matlab_src with new version.
 mv $D4M_HOME/DBinit.m $D4M_HOME/matlab_src

7802107d807f6499941ddb5a7c8cbc0ce1b25fc5
Dylan Hutchison <dhutchis@mit.edu>
2015-05-25 17:36:33 -0400

    implement writing RCV files to Accumulo. No tests yet.

---
+++ graphulo/src/main/java/edu/mit/ll/graphulo/d4m/D4MTableWriter.java
@@ -0,0 +1,277 @@
+package edu.mit.ll.graphulo.d4m;
+
+import org.apache.accumulo.core.client.*;
+import org.apache.accumulo.core.client.admin.TableOperations;
+import org.apache.accumulo.core.data.Mutation;
+import org.apache.accumulo.core.data.Value;
+import org.apache.accumulo.core.iterators.Combiner;
+import org.apache.accumulo.core.iterators.IteratorUtil;
+import org.apache.accumulo.core.iterators.LongCombiner;
+import org.apache.accumulo.core.iterators.user.SummingCombiner;
+import org.apache.hadoop.io.Text;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+/**
+ * Wrapper around the following tables:
+ * table,
+ * tableT,
+ * tableDeg,
+ * tableTDeg.
+ * Can set the column family if you want to use a non-empty one.
+ */
+public class D4MTableWriter implements AutoCloseable {
+  private static final Logger log = LogManager.getLogger(D4MTableWriter.class);
+
+  @Override
+  public void close() {
+    this.closeIngest();
+  }
+
+  enum State { New, Open, Closed }
+  private State state = State.New;
+
+  public static final Text DEFAULT_DEGCOL = new Text("deg");
+  public static final Value VALONE = new Value("1".getBytes());
+  public static final Text EMPTYCF =new Text("");
+
+  /** Holds configuration options to pass to constructor of D4MTableWriter. */
+  public static class D4MTableConfig implements Cloneable {
+    public String baseName;
+    public Connector connector;
+    public boolean
+        useTable = false,
+        useTableT = false,
+        useTableDeg = false,
+        useTableDegT = false,
+        useTableField = false,
+        useTableFieldT = false;
+    public Text textDegCol = DEFAULT_DEGCOL;
+    public Text cf = EMPTYCF;
+    /** The number of bytes until we flush data to the server. */
+    public long batchBytes = 2_000_000L;
+
+    public D4MTableConfig() {}
+
+    public D4MTableConfig(D4MTableConfig c) {
+      baseName = c.baseName;
+      connector = c.connector;
+      useTable = c.useTable;
+      useTableT = c.useTableT;
+      useTableDeg = c.useTableDeg;
+      useTableDegT = c.useTableDegT;
+      useTableField = c.useTableField;
+      useTableFieldT = c.useTableFieldT;
+      textDegCol = c.textDegCol;
+      batchBytes = c.batchBytes;
+      cf = c.cf;
+    }
+  }
+  private final D4MTableConfig tconf;
+
+  private String TNtable,TNtableT,TNtableDeg, TNtableDegT, TNtableField, TNtableFieldT;
+  private BatchWriter
+      Btable=null,
+      BtableT=null,
+      BtableDeg=null,
+      BtableDegT =null,
+      BtableField=null,
+      BtableFieldT=null;
+  private MultiTableBatchWriter mtbw;
+
+
+
+  private static final String ITER_SUMALL_NAME = "sumAll";
+
+  /** Put a SummingIterator on all columns. */
+  public static void assignDegreeAccumulator(String tableName, Connector c) {
+    IteratorSetting cfg = null;
+    try {
+      cfg = c.tableOperations().getIteratorSetting(tableName, ITER_SUMALL_NAME, IteratorUtil.IteratorScope.scan);
+    } catch (AccumuloSecurityException | AccumuloException ignored) {
+
+    } catch (TableNotFoundException e) {
+      log.warn(tableName + " does not exist", e);
+    }
+
+    if (cfg != null) {
+      log.info("table "+tableName+": iterator "+ITER_SUMALL_NAME+" already exists with priority "+cfg.getPriority()+" and options: "+cfg.getOptions());
+
+    } else {
+      cfg = new IteratorSetting(19, ITER_SUMALL_NAME, SummingCombiner.class);
+      //Combiner.setColumns(cfg, columnList);
+      Combiner.setCombineAllColumns(cfg, true);
+      LongCombiner.setEncodingType(cfg, LongCombiner.Type.STRING);
+      try {
+        //c.tableOperations().checkIteratorConflicts(tableName, cfg, EnumSet.allOf(IteratorUtil.IteratorScope.class));
+        c.tableOperations().attachIterator(tableName, cfg);
+      } catch (AccumuloSecurityException | AccumuloException e) {
+        log.warn("error trying to add "+ITER_SUMALL_NAME+" iterator to " + tableName, e);
+      } catch (TableNotFoundException e) {
+        log.warn(tableName + " does not exist", e);
+      }
+    }
+  }
+
+  /** Create a table if not already existing. Return whether table created. */
+  public static boolean createTableSoft(String tableName, Connector c) {
+    TableOperations to = c.tableOperations();
+    try {
+      if (!to.exists(tableName)) {
+        to.create(tableName);
+        return true;
+      } else
+        return false;
+    } catch (AccumuloException | AccumuloSecurityException e) {
+      log.warn("error creating table "+tableName,e);
+      return false;
+    } catch (TableExistsException e) {
+      log.error("impossible! Table checked to be created!", e);
+      return false;
+    }
+  }
+
+  /** All values from the config object are copied. */
+  public D4MTableWriter(D4MTableConfig config) {
+    tconf = new D4MTableConfig(config); // no aliasing
+    initBaseBames(tconf.baseName);
+    openIngest();
+  }
+
+  private void initBaseBames(String baseName) {
+    if (tconf.useTable)     TNtable=baseName;
+    if (tconf.useTableT)    TNtableT=baseName+"T";
+    if (tconf.useTableDeg)  TNtableDeg=baseName+"Deg";
+    if (tconf.useTableDegT) TNtableDegT =baseName + "DegT";
+    if (tconf.useTableField) TNtableField =baseName + "Field";
+    if (tconf.useTableFieldT) TNtableFieldT =baseName + "FieldT";
+  }
+
+
+  /**
+   * Create the tables to ingest to if they do not already exist.
+   * Sets up iterators on degree tables if enabled.
+   */
+  public void createTablesSoft() {
+    boolean btDeg=false, btDegT=false, btField=false, btFieldT=false;
+    if (tconf.useTable)     createTableSoft(TNtable, tconf.connector);
+    if (tconf.useTableT)     createTableSoft(TNtableT, tconf.connector);
+    if (tconf.useTableDeg)  btDeg = createTableSoft(TNtableDeg, tconf.connector);
+    if (tconf.useTableDegT) btDegT = createTableSoft(TNtableDegT, tconf.connector);
+    if (tconf.useTableField) btField = createTableSoft(TNtableField, tconf.connector);
+    if (tconf.useTableFieldT) btFieldT = createTableSoft(TNtableFieldT, tconf.connector);
+    //List<IteratorSetting.Column> columns = Collections.singletonList(new IteratorSetting.Column(tconf.cf, tconf.textDegCol));
+    if (btDeg)  assignDegreeAccumulator(TNtableDeg, tconf.connector);
+    if (btDegT) assignDegreeAccumulator(TNtableDegT, tconf.connector);
+    if (btField) assignDegreeAccumulator(TNtableField, tconf.connector);
+    if (btFieldT) assignDegreeAccumulator(TNtableFieldT, tconf.connector);
+  }
+
+  public void openIngest() {
+    switch(state) {
+      case New: createTablesSoft(); break;
+      case Open: throw new IllegalStateException("tried to open ingset when already open");
+      case Closed: break;
+    }
+
+    BatchWriterConfig BWconfig = new BatchWriterConfig();
+    BWconfig.setMaxMemory(tconf.batchBytes);
+    mtbw = tconf.connector.createMultiTableBatchWriter(BWconfig);
+    try {
+      if (tconf.useTable) Btable         = mtbw.getBatchWriter(TNtable);
+      if (tconf.useTableT) BtableT       = mtbw.getBatchWriter(TNtableT);
+      if (tconf.useTableDeg) BtableDeg   = mtbw.getBatchWriter(TNtableDeg);
+      if (tconf.useTableDegT) BtableDegT = mtbw.getBatchWriter(TNtableDegT);
+      if (tconf.useTableField) BtableField = mtbw.getBatchWriter(TNtableField);
+      if (tconf.useTableFieldT) BtableFieldT = mtbw.getBatchWriter(TNtableFieldT);
+    } catch (TableNotFoundException e) {
+      log.error("impossible! Tables should have been created!", e);
+    } catch (AccumuloSecurityException | AccumuloException e) {
+      log.warn("error creating one of the batch writers for D4MTableWriter base " + TNtable, e);
+    }
+    state = State.Open;
+  }
+
+  public void flushBuffers() {
+    if (state != State.Open)
+      throw new IllegalStateException("flushing buffer when not open");
+    try {
+      mtbw.flush();
+    } catch (MutationsRejectedException e) {
+      log.warn("mutations rejected while flushing",e);
+    }
+  }
+
+  /**
+   * Close all enabled table batch writers.
+   */
+  public void closeIngest() {
+    if (state != State.Open)
+      throw new IllegalStateException("tried to close when already closed");
+    Btable     = null;
+    BtableT    = null;
+    BtableDeg  = null;
+    BtableDegT = null;
+    BtableField = null;
+    BtableFieldT = null;
+    try {
+      mtbw.close();
+    } catch (MutationsRejectedException e) {
+      log.warn("error closing multi table writer for D4MTableWriter",e);
+    }
+    state = State.Closed;
+  }
+
+  @Override
+  public void finalize() throws Throwable {
+    super.finalize();
+    if (state == State.Open)
+      closeIngest();
+  }
+
+  public static final char FIELD_SEPERATOR = '|';
+
+  /** Use "1" as the Value. */
+  public void ingestRow(Text rowID, Text cq) {
+    ingestRow(rowID, cq, VALONE);
+  }
+  /** Ingest to all enabled tables. Use "1" for the degree table values. */
+  public void ingestRow(Text rowID, Text cq, Value v) {
+    if (state != State.Open)
+      openIngest();
+    if (tconf.useTable)     ingestRow(Btable    , rowID, tconf.cf, cq, v);
+    if (tconf.useTableT)    ingestRow(BtableT   , cq, tconf.cf, rowID, v);
+    if (tconf.useTableDeg)  ingestRow(BtableDeg , rowID, tconf.cf, tconf.textDegCol, VALONE);
+    if (tconf.useTableDegT) ingestRow(BtableDegT, cq, tconf.cf, tconf.textDegCol, VALONE);
+    if (tconf.useTableField) {
+      String rowIDString = rowID.toString();
+      int fieldSepPos;
+      if ((fieldSepPos = rowIDString.indexOf(FIELD_SEPERATOR)) == -1)
+        log.warn(TNtableField +" is turned on, but the row "+rowIDString+" to ingest does not have a field seperator "+FIELD_SEPERATOR);
+      else {
+        Text rowIDField = new Text(rowIDString.substring(0, fieldSepPos));
+        ingestRow(BtableField, rowIDField, tconf.cf, tconf.textDegCol, VALONE);
+      }
+    }
+    if (tconf.useTableFieldT){
+      String cqString = cq.toString();
+      int fieldSepPos;
+      if ((fieldSepPos = cqString.indexOf(FIELD_SEPERATOR)) == -1)
+        log.warn(TNtableFieldT +" is turned on, but the row "+cqString+" to ingest does not have a field seperator "+FIELD_SEPERATOR);
+      else {
+        Text cqField = new Text(cqString.substring(0, fieldSepPos));
+        ingestRow(BtableFieldT, cqField, tconf.cf, tconf.textDegCol, VALONE);
+      }
+    }
+  }
+
+  public static void ingestRow(BatchWriter bw, Text rowID, Text cf, Text cq, Value v) {
+    Mutation m = new Mutation(rowID);
+    m.put(cf, cq, v);
+    try {
+      bw.addMutation(m);
+    } catch (MutationsRejectedException e) {
+      log.warn("mutation rejected: (row,cf,cq,v)=("+rowID+','+cf+','+cq+','+v+")",e);
+    }
+  }
+}
---
+++ graphulo/src/main/java/edu/mit/ll/graphulo/d4m/D4MTripleWriter.java
@@ -0,0 +1,94 @@
+package edu.mit.ll.graphulo.d4m;
+
+import org.apache.accumulo.core.client.Connector;
+import org.apache.accumulo.core.data.Value;
+import org.apache.hadoop.io.Text;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Write row, column and (optionally) value files to a table.
+ */
+public class D4MTripleWriter {
+
+  private Connector connector;
+
+  public D4MTripleWriter(Connector connector) {
+    this.connector = connector;
+  }
+
+  /**
+   * Writes triples from component files to a main table, transpose table and degree table.
+   *
+   * @param valFile Optional value file. Uses "1" if not given.
+   * @param delimiter Delimiter that separates items.
+   * @param baseName Name of tables is the base name plus "", "T", "Deg"
+   * @param trackTime Log the rate of ingest or not.
+   * @return Number of triples written.
+   * @throws FileNotFoundException
+   */
+  public long writeTripleFile(File rowFile, File colFile, File valFile,
+                              String delimiter, String baseName,
+                              boolean trackTime) throws FileNotFoundException {
+    long count = 0, startTime, origStartTime;
+    Text row = new Text(), col = new Text(), valText = null;
+    Value val = D4MTableWriter.VALONE;
+
+    Scanner valScanner = null;
+    try (Scanner rowScanner = new Scanner(rowFile);
+         Scanner colScanner = new Scanner(colFile)) {
+      rowScanner.useDelimiter(delimiter);
+      colScanner.useDelimiter(delimiter);
+      if (valFile != null) {
+        valScanner = new Scanner(valFile);
+        valScanner.useDelimiter(delimiter);
+        valText = new Text();
+      }
+
+      D4MTableWriter.D4MTableConfig config = new D4MTableWriter.D4MTableConfig();
+      config.baseName = baseName;
+      config.useTable = config.useTableT = true;
+      config.useTableDeg = true;
+      config.useTableDegT = false;
+      config.useTableField = false;
+      config.useTableFieldT = false;
+      config.connector = connector;
+
+      origStartTime = startTime = System.currentTimeMillis();
+      try (D4MTableWriter tw = new D4MTableWriter(config)) {
+        while (rowScanner.hasNext()) {
+          if (!colScanner.hasNext() || (valScanner != null && !valScanner.hasNext())) {
+            throw new IllegalArgumentException("row, col and val files do not have the same number of elements. " +
+                " rowScanner.hasNext()=" + rowScanner.hasNext() +
+                " colScanner.hasNext()=" + colScanner.hasNext() +
+                (valScanner == null ? "" : " valScanner.hasNext()=" + valScanner.hasNext()));
+          }
+          row.set(rowScanner.next());
+          col.set(colScanner.next());
+          if (valFile != null) {
+            valText.set(valScanner.next());
+            val.set(valText.getBytes());
+          }
+          tw.ingestRow(row, col, val);
+          count++;
+          
+          if (trackTime && count % 100000 == 0) {
+            long stopTime = System.currentTimeMillis();
+            if (startTime - stopTime > 1000*60) {
+              System.out.printf("Ingest: %9d cnt, %6d secs, %8d entries/sec\n", count, (stopTime - origStartTime)/1000,
+                  Math.round(count / ((stopTime - origStartTime)/1000.0)));
+              startTime = stopTime;
+            }
+          }
+        }
+      }
+    } finally {
+      if (valScanner != null)
+        valScanner.close();
+    }
+    return count;
+  }
+
+}

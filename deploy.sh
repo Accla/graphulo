b65822020aa8db553840fba6d6da2c1c8b2b86af
Dylan Hutchison <dhutchis@mit.edu>
2015-07-27 16:01:52 -0400

    Make deploy.sh robust to ACCUMULO_HOME and D4M_HOME

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,18 @@ set -e #command fail -> script fail
 set -u #unset variable reference causes script fail
 
 # Install server-side iterators in Accumulo.
-cp target/graphulo-*.jar "$ACCUMULO_HOME/lib/ext"
+if [ -z ${ACCUMULO_HOME+x} ]; then
+  echo "Not installing Graphulo JAR in Accumulo server because ACCUMULO_HOME is not set";
+else
+  cp target/graphulo-*.jar "$ACCUMULO_HOME/lib/ext"
+fi
 
 # Install client code + dependencies in D4M.
-cp target/graphulo-*.jar "$D4M_HOME/lib"
-unzip -ouq target/graphulo-*-libext.zip -d "$D4M_HOME"
-# Replace DBinit.m in D4M matlab_src with new version.
-mv "$D4M_HOME/DBinit.m" "$D4M_HOME/matlab_src"
+if [ -z ${D4M_HOME+x} ]; then
+  echo "Not installing Graphulo JAR in D4M installation because D4M_HOME is not set";
+else
+  cp target/graphulo-*.jar "$D4M_HOME/lib"
+  unzip -ouq target/graphulo-*-libext.zip -d "$D4M_HOME"
+  # Replace DBinit.m in D4M matlab_src with new version.
+  mv "$D4M_HOME/DBinit.m" "$D4M_HOME/matlab_src"
+fi

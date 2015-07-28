54199354acdc4e951da3e5f4ee9a05cf11230259
Dylan Hutchison <dhutchis@mit.edu>
2015-07-28 11:59:21 -0400

    adjust TEST_CONFIG to txe1 architecture

--- src/test/java/edu/mit/ll/graphulo/TEST_CONFIG.java
+++ src/test/java/edu/mit/ll/graphulo/TEST_CONFIG.java
@@ -7,6 +7,12 @@ import org.apache.log4j.Logger;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 @RunWith(KnownBugSuite.class)
 @Suite.SuiteClasses({
     UtilTest.class,
@@ -39,32 +45,46 @@ public class TEST_CONFIG {
     String s = System.getProperty("TEST_CONFIG"); // environment variable
     if (s == null)
       s = "mini";
-    switch (s) {
-      case "local":
-        AccumuloTester = new RealAccumuloTester("instance", "localhost:2181", 5000, "root", new PasswordToken("secret"));
-        break;
-      case "local-1.7.0":
-        AccumuloTester = new RealAccumuloTester("instance-1.7.0", "localhost:2181", 5000, "root", new PasswordToken("secret"));
-        break;
-      case "txe1":
-        AccumuloTester = new RealAccumuloTester("classdb51", "classdb51.cloud.llgrid.txe1.mit.edu:2181", 5000, "AccumuloUser", new PasswordToken("secret"));
-        break;
-      case "mini":
-        AccumuloTester = new MiniAccumuloTester(1, false, false);
-        break;
-      case "miniDebug":   // Enables debugging on started MiniAccumulo process.
-        AccumuloTester = new MiniAccumuloTester(1, true, false);
-        break;
-      case "mini2": // 2 tablet server MiniAccumuloCluster
-        AccumuloTester = new MiniAccumuloTester(2, false, false);
-        break;
-      case "miniReuse":
-        AccumuloTester = new MiniAccumuloTester(2, false, true);
-        break;
-      default:
-        log.warn("Using \"mini\" due to unrecognized TEST_ACCUMULO option: " + s);
-        AccumuloTester = new MiniAccumuloTester(1, false, false);
-        break;
+    if (s.startsWith("txe1-")) {
+      String instance  = s.substring(5);
+      File file = new File("/home/gridsan/groups/databases/"+instance+"/accumulo_user_password.txt");
+      PasswordToken token;
+      try (BufferedReader is = new BufferedReader(new FileReader(file))) {
+        token = new PasswordToken(is.readLine());
+      } catch (FileNotFoundException e) {
+        log.error("Cannot find accumulo_user_password.txt for instance "+instance, e);
+        throw new RuntimeException(e);
+      } catch (IOException e) {
+        log.error("Problem reading accumulo_user_password.txt for instance " + instance, e);
+        throw new RuntimeException(e);
+      }
+      AccumuloTester = new RealAccumuloTester(instance, instance+".cloud.llgrid.txe1.mit.edu:2181", 5000, "AccumuloUser", token);
+
+    } else {
+      switch (s) {
+        case "local":
+          AccumuloTester = new RealAccumuloTester("instance", "localhost:2181", 5000, "root", new PasswordToken("secret"));
+          break;
+        case "local-1.7.0":
+          AccumuloTester = new RealAccumuloTester("instance-1.7.0", "localhost:2181", 5000, "root", new PasswordToken("secret"));
+          break;
+        case "mini":
+          AccumuloTester = new MiniAccumuloTester(1, false, false);
+          break;
+        case "miniDebug":   // Enables debugging on started MiniAccumulo process.
+          AccumuloTester = new MiniAccumuloTester(1, true, false);
+          break;
+        case "mini2": // 2 tablet server MiniAccumuloCluster
+          AccumuloTester = new MiniAccumuloTester(2, false, false);
+          break;
+        case "miniReuse":
+          AccumuloTester = new MiniAccumuloTester(2, false, true);
+          break;
+        default:
+          log.warn("Using \"mini\" due to unrecognized TEST_ACCUMULO option: " + s);
+          AccumuloTester = new MiniAccumuloTester(1, false, false);
+          break;
+      }
     }
   }
 

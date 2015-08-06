bfbd2b4dcba8226ff25ba2d628144fc9f0b21d1c
Dylan Hutchison <dhutchis@mit.edu>
2015-08-06 03:09:39 -0400

    Change log4j strategy to append to console at DEBUG level.
    Got rid of file appender in the silly logs/ folder.
    Will experiment with what logging/notifying user works best.

--- src/test/java/edu/mit/ll/graphulo/TEST_CONFIG.java
+++ src/test/java/edu/mit/ll/graphulo/TEST_CONFIG.java
@@ -63,6 +63,8 @@ public class TEST_CONFIG {
     } else {
       switch (s) {
         case "local":
+          // G = edu.mit.ll.graphulo.MatlabGraphulo('instance','localhost:2181','root','secret')
+          // DB = DBserver('localhost:2181','Accumulo','instance','root','secret')
           AccumuloTester = new RealAccumuloTester("instance", "localhost:2181", 5000, "root", new PasswordToken("secret"));
           break;
         case "local-1.7.0":

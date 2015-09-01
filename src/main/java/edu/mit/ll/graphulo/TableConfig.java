de757433a265de9cde289d1d521fbbcaa3cff4ea
Dylan Hutchison <dhutchis@mit.edu>
2015-09-01 01:56:05 -0400

    no need to explicitly track token class. Add 'with' methods

--- src/main/java/edu/mit/ll/graphulo/TableConfig.java
+++ src/main/java/edu/mit/ll/graphulo/TableConfig.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.apache.accumulo.core.client.ClientConfiguration.ClientProperty;
@@ -29,7 +30,6 @@ public final class TableConfig implements Serializable {
   private final String tableName;
   private final String username;
   private final transient AuthenticationToken authenticationToken; // clone this
-  private final Class<? extends AuthenticationToken> authenticationTokenClass;
   private final Authorizations authorizations; // immutable and Serializable
   private final String rowRanges;
   private final String colFilter;
@@ -40,11 +40,14 @@ public final class TableConfig implements Serializable {
 
   private void writeObject(java.io.ObjectOutputStream out) throws IOException {
     out.defaultWriteObject();
+    out.writeObject(authenticationToken.getClass()); // store class information so we can recover the class name on read
     authenticationToken.write(out);
   }
 
+  @SuppressWarnings("unchecked")
   private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
     in.defaultReadObject();
+    Class<? extends AuthenticationToken> authenticationTokenClass = (Class<? extends AuthenticationToken>)in.readObject();
     // small hack that enables setting a final variable
     try {
       Field authField = TableConfig.class.getDeclaredField("authenticationToken");
@@ -58,30 +61,65 @@ public final class TableConfig implements Serializable {
     authenticationToken.readFields(in);
   }
 
-  public TableConfig(String tableName, String username, AuthenticationToken authenticationToken) {
+  public TableConfig(String tableName, String username,
+                     AuthenticationToken authenticationToken) {
     this(ClientConfiguration.loadDefault(), tableName, username, authenticationToken);
   }
 
-  public TableConfig(ClientConfiguration cc, String tableName, String username, AuthenticationToken authenticationToken) {
-    if (cc == null) cc = ClientConfiguration.loadDefault();
-    instanceName = cc.get(ClientProperty.INSTANCE_NAME); // default: null
+  public TableConfig(ClientConfiguration cc, String tableName, String username,
+                     AuthenticationToken authenticationToken) {
+    this(cc.get(ClientProperty.INSTANCE_ZK_HOST), cc.get(ClientProperty.INSTANCE_NAME),
+        AccumuloConfiguration.getTimeInMillis(cc.get(ClientProperty.INSTANCE_ZK_TIMEOUT)),
+        tableName, username, authenticationToken);
+  }
+
+  public TableConfig(String zookeeperHost, String instanceName, String tableName, String username,
+                     AuthenticationToken authenticationToken) {
+    this(zookeeperHost, instanceName,
+        AccumuloConfiguration.getTimeInMillis(ClientProperty.INSTANCE_ZK_HOST.getDefaultValue()),
+        tableName, username, authenticationToken);
+  }
+
+  public TableConfig(String zookeeperHost, String instanceName, long timeout, String tableName, String username,
+                     AuthenticationToken authenticationToken) {
     Preconditions.checkNotNull(instanceName,
         "No instance name provided and none found in the default ClientConfiguration (usually loaded from a client.conf file)");
-    zookeeperHost = cc.get(ClientProperty.INSTANCE_ZK_HOST); // default: "localhost:2181"
-    timeout = AccumuloConfiguration.getTimeInMillis(cc.get(ClientProperty.INSTANCE_ZK_TIMEOUT)); // default: "30s"
+    this.instanceName = instanceName; // default: null
+    Preconditions.checkNotNull(zookeeperHost);
+    this.zookeeperHost = zookeeperHost; // default: "localhost:2181"
+    this.timeout = timeout; // default: "30s"
     Preconditions.checkNotNull(tableName);
     this.tableName = tableName;
     Preconditions.checkNotNull(username);
     this.username = username;
     Preconditions.checkNotNull(authenticationToken, "authenticationToken must be specified not null for user %s", username);
     this.authenticationToken = authenticationToken.clone();
-    authenticationTokenClass = this.authenticationToken.getClass();
     authorizations = Authorizations.EMPTY;
     rowRanges = null;
     colFilter = null;
     doClientSideIterators = false;
     itersAfter = itersBefore = Collections.emptyMap();
     priority = 21; // arbitrary 21
+  }
+
+  private TableConfig(int priority, String zookeeperHost, long timeout, String instanceName,
+                     String tableName, String username, AuthenticationToken authenticationToken,
+                      Authorizations authorizations, String rowRanges, String colFilter,
+                      boolean doClientSideIterators,
+                      Map<String, String> itersBefore, Map<String, String> itersAfter) {
+    this.priority = priority;
+    this.zookeeperHost = zookeeperHost;
+    this.timeout = timeout;
+    this.instanceName = instanceName;
+    this.tableName = tableName;
+    this.username = username;
+    this.authenticationToken = authenticationToken.clone();
+    this.authorizations = authorizations;
+    this.rowRanges = rowRanges;
+    this.colFilter = colFilter;
+    this.doClientSideIterators = doClientSideIterators;
+    this.itersBefore = itersBefore;
+    this.itersAfter = itersAfter;
   }
 
   @Override
@@ -129,6 +167,78 @@ public final class TableConfig implements Serializable {
     result = 31 * result + itersBefore.hashCode();
     result = 31 * result + itersAfter.hashCode();
     return result;
+  }
+
+  public TableConfig withInstanceName(String instanceName) {
+    Preconditions.checkNotNull(instanceName);
+    return new TableConfig(priority, zookeeperHost, timeout, instanceName,
+        tableName, username, authenticationToken, authorizations, rowRanges, colFilter,
+        doClientSideIterators, itersBefore, itersAfter);
+  }
+
+  public TableConfig withZookeeperTimeout(long timeout) {
+    return new TableConfig(priority, zookeeperHost, timeout, instanceName,
+        tableName, username, authenticationToken, authorizations, rowRanges, colFilter,
+        doClientSideIterators, itersBefore, itersAfter);
+  }
+
+  public TableConfig withZookeeperHost(String zookeeperHost) {
+    Preconditions.checkNotNull(zookeeperHost);
+    return new TableConfig(priority, zookeeperHost, timeout, instanceName,
+        tableName, username, authenticationToken, authorizations, rowRanges, colFilter,
+        doClientSideIterators, itersBefore, itersAfter);
+  }
+
+  public TableConfig withTableName(String tableName) {
+    Preconditions.checkNotNull(tableName);
+    return new TableConfig(priority, zookeeperHost, timeout, instanceName,
+        tableName, username, authenticationToken, authorizations, rowRanges, colFilter,
+        doClientSideIterators, itersBefore, itersAfter);
+  }
+  public TableConfig withUsername(String username) {
+    Preconditions.checkNotNull(username);
+    return new TableConfig(priority, zookeeperHost, timeout, instanceName,
+        tableName, username, authenticationToken, authorizations, rowRanges, colFilter,
+        doClientSideIterators, itersBefore, itersAfter);
+  }
+  public TableConfig withAuthenticationToken(AuthenticationToken authenticationToken) {
+    Preconditions.checkNotNull(authenticationToken);
+    return new TableConfig(priority, zookeeperHost, timeout, instanceName,
+        tableName, username, authenticationToken, authorizations, rowRanges, colFilter,
+        doClientSideIterators, itersBefore, itersAfter);
+  }
+  public TableConfig withAuthorizations(Authorizations authorizations) {
+    Preconditions.checkNotNull(authorizations);
+    return new TableConfig(priority, zookeeperHost, timeout, instanceName,
+        tableName, username, authenticationToken, authorizations, rowRanges, colFilter,
+        doClientSideIterators, itersBefore, itersAfter);
+  }
+  public TableConfig withRowRanges(String rowRanges) {
+    return new TableConfig(priority, zookeeperHost, timeout, instanceName,
+        tableName, username, authenticationToken, authorizations, rowRanges, colFilter,
+        doClientSideIterators, itersBefore, itersAfter);
+  }
+  public TableConfig withColFilter(String colFilter) {
+    return new TableConfig(priority, zookeeperHost, timeout, instanceName,
+        tableName, username, authenticationToken, authorizations, rowRanges, colFilter,
+        doClientSideIterators, itersBefore, itersAfter);
+  }
+  public TableConfig withDoClientSideIterators(boolean doClientSideIterators) {
+    return new TableConfig(priority, zookeeperHost, timeout, instanceName,
+        tableName, username, authenticationToken, authorizations, rowRanges, colFilter,
+        doClientSideIterators, itersBefore, itersAfter);
+  }
+  public TableConfig withItersBefore(Map<String,String> itersBefore) {
+    Preconditions.checkNotNull(instanceName);
+    return new TableConfig(priority, zookeeperHost, timeout, instanceName,
+        tableName, username, authenticationToken, authorizations, rowRanges, colFilter,
+        doClientSideIterators, Collections.unmodifiableMap(new HashMap<>(itersBefore)), itersAfter);
+  }
+  public TableConfig withItersAfter(Map<String,String> itersAfter) {
+    Preconditions.checkNotNull(instanceName);
+    return new TableConfig(priority, zookeeperHost, timeout, instanceName,
+        tableName, username, authenticationToken, authorizations, rowRanges, colFilter,
+        doClientSideIterators, itersBefore, Collections.unmodifiableMap(new HashMap<>(itersAfter)));
   }
 
   public int getPriority() {

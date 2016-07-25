ec07c94a0366b54d708db6b1f01ee456915dd9d1
Dylan Hutchison <dhutchis@cs.washington.edu>
2016-07-25 07:35:30 -0700

    Add ReverseComplementIterator

--- src/test/java/edu/mit/ll/graphulo_ocean/GenomicEncoderTest.java
+++ src/test/java/edu/mit/ll/graphulo_ocean/GenomicEncoderTest.java
@@ -1,9 +1,28 @@
 package edu.mit.ll.graphulo_ocean;
 
+import edu.mit.ll.graphulo.DynamicIteratorSetting;
+import edu.mit.ll.graphulo.skvi.D4mRangeFilter;
+import edu.mit.ll.graphulo.skvi.DynamicIterator;
+import edu.mit.ll.graphulo.skvi.MapIterator;
+import org.apache.accumulo.core.client.IteratorSetting;
+import org.apache.accumulo.core.client.mock.IteratorAdapter;
+import org.apache.accumulo.core.data.ByteSequence;
+import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.Range;
+import org.apache.accumulo.core.data.Value;
+import org.apache.accumulo.core.iterators.SortedKeyValueIterator;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import static edu.mit.ll.graphulo.UtilTest.mockIteratorEnvrironment;
+import static edu.mit.ll.graphulo.util.GraphuloUtil.EMPTY_BYTES;
 import static edu.mit.ll.graphulo_ocean.GenomicEncoder.reverseComplement;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * Test encoding, decoding of k-mer character arrays to/from byte arrays.
@@ -69,8 +88,43 @@ public class GenomicEncoderTest {
         new byte[][] { g3.encode(new char[] {'T','C','C'}), g3.reverseComplement(g3.encode(new char[] {'G','G','A'})) },
         new byte[][] { g3.encode(new char[] {'T','C','G'}), g3.reverseComplement(g3.encode(new char[] {'C','G','A'})) },
         new byte[][] { g5.encode(new char[] {'A','A','C','T','G'}), g5.reverseComplement(g5.encode(new char[] {'C','A','G','T','T'})) },
-        new byte[][] { g5.encode(new char[] {'C','G','A','A','C','T','G'}), g5.reverseComplement(g5.encode(new char[] {'C','A','G','T','T','C','G'})) }
+        new byte[][] { g7.encode(new char[] {'C','G','A','A','C','T','G'}), g7.reverseComplement(g7.encode(new char[] {'C','A','G','T','T','C','G'})) }
     };
+  }
+
+  @Test
+  public void testReverseComplementIterator() throws Exception {
+    IteratorSetting itset = new DynamicIteratorSetting(1, "hi")
+        .append(ReverseComplementApply.iteratorSetting(1, D4mRangeFilter.KeyPart.COLQ, 5))
+        .toIteratorSetting();
+    Map<String,String> opts = itset.getOptions();
+
+    GenomicEncoder g5 = new GenomicEncoder(5);
+    SortedMap<Key,Value> input = new TreeMap<>();
+    input.put(new Key("r1".getBytes(UTF_8), EMPTY_BYTES, g5.encode(new char[] {'A','A','C','T','G'}), EMPTY_BYTES, 1, false, false), new Value("1".getBytes(UTF_8)));
+
+    SortedKeyValueIterator<Key,Value> skvi = new MapIterator(input);
+    skvi.init(null, null, null);
+    SortedKeyValueIterator<Key,Value> skviTop = new DynamicIterator();
+    skviTop.init(skvi, opts, mockIteratorEnvrironment(DynamicIteratorSetting.MyIteratorScope.MINC));
+    skvi = skviTop;
+    skvi.seek(new Range(), Collections.<ByteSequence>emptySet(), false);
+
+    SortedMap<Key,Value> expect = new TreeMap<>();
+    expect.put(new Key("r1".getBytes(UTF_8), EMPTY_BYTES, g5.reverseComplement(g5.encode(new char[] {'A','A','C','T','G'})), EMPTY_BYTES, 1, false, false), new Value("1".getBytes(UTF_8)));
+
+    IteratorAdapter ia = new IteratorAdapter(skvi);
+    //    while (ia.hasNext()) {
+//      Map.Entry<Key, Value> next = ia.next();
+//      System.out.println(next.getKey().toString()+" -> "+next.getValue());
+//    }
+    for (Map.Entry<Key, Value> expectEntry : expect.entrySet()) {
+      Assert.assertTrue(ia.hasNext());
+      Map.Entry<Key, Value> actualEntry = ia.next();
+      Assert.assertEquals(expectEntry, actualEntry);
+    }
+    Assert.assertFalse(ia.hasNext());
+
   }
 
 }

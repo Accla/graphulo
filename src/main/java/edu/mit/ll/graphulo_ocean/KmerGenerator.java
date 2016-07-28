4700a6010e23be9823d5f8d51b354a04976674e7
Dylan Hutchison <dhutchis@cs.washington.edu>
2016-07-28 06:52:58 -0700

    Kmer generator more realistic; random reads

--- src/main/java/edu/mit/ll/graphulo_ocean/KmerGenerator.java
+++ src/main/java/edu/mit/ll/graphulo_ocean/KmerGenerator.java
@@ -5,6 +5,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 /**
@@ -16,25 +19,59 @@ import java.util.Random;
 public class KmerGenerator {
 
   private final int K;
-  private final char[][] GC_DEC;
+//  private final char[][] GC_DEC;
 
   public KmerGenerator(int k) {
     K = k;
-    GC_DEC = new char[K+1][];
-    for (int i = 0; i < K + 1; i++) {
-      GC_DEC[i] = Double.toString(((double)i)/K).toCharArray();
-    }
+//    GC_DEC = new char[K+1][];
+//    for (int i = 0; i < K + 1; i++) {
+//      GC_DEC[i] = Double.toString(((double)i)/K).toCharArray();
+//    }
   }
 
   public static void main(String[] args) throws IOException {
-    File f = new File("kmer_random_norm03.csv");
+    File f = new File("kmer_random_norm04.csv");
+    String sampleid = "Srand04";
     KmerGenerator kgen = new KmerGenerator(11);
+    int[] countMap = null;
     try (PrintWriter fw = new PrintWriter(new BufferedWriter(new FileWriter(f)))) {
-      kgen.generateKmerCounts(fw, "Srand03", 963323535, true, 0.417);
+//      kgen.generateKmerCounts(fw, "Srand03", 963323535, true, 0.417, 25, 200);
+      countMap = kgen.generateKmerCounts_Restart(fw, sampleid, 963323535, 93323535, true, 0.417, 25, 200);
+    }
+
+    File f2 = new File("kmer_random_norm04_degreedist.csv");
+    try (PrintWriter fw = new PrintWriter(new BufferedWriter(new FileWriter(f2)))) {
+      writeCountMap(fw, sampleid, countMap, 963323535);
     }
   }
 
-  private final Random R = new Random(2016072701);
+  private static void writeCountMap(PrintWriter pw, String sampleid, int[] countMap, int expectedNumkmers) {
+    Map<Integer, Integer> degMap = new HashMap<>();
+    int numkmers = 0;
+    for (int i : countMap) {
+      int kmers = i;
+      numkmers += kmers;
+      Integer cnt = degMap.get(kmers);
+      if (cnt == null)
+        degMap.put(kmers, 1);
+      else
+        degMap.put(kmers, cnt+1);
+    }
+    if (numkmers != expectedNumkmers)
+     System.out.println("Warning: expected "+expectedNumkmers+" but found numkmers: "+numkmers);
+
+    pw.println("sampleid,norm_cnt,cnt_norm_cnt");
+    final String sidcomma = sampleid+",";
+    for (Map.Entry<Integer, Integer> entry : degMap.entrySet()) {
+      pw.print(sidcomma);
+      pw.print(((double)entry.getKey())/numkmers);
+      pw.print(',');
+      pw.println(entry.getValue());
+    }
+  }
+
+  private final Random R = new Random(2016072801);
+  // 20160726, 2016072602, 2016072701, 2016072801
 
   // 0, 1, 2, 3: G, C, A, T
   private byte genChar(double fix_gc) {
@@ -57,7 +94,7 @@ public class KmerGenerator {
     }
   }
 
-  private void generateKmerCounts(PrintWriter pw, String sampleid, int numkmers, boolean normalize, double fix_gc) {
+  private void generateKmerCounts(PrintWriter pw, String sampleid, int numkmers, boolean normalize, double fix_gc, double restartStdev, double restartMean) {
     byte[] buf = new byte[10000];
     int[] countMap = new int[1 << (K << 1)];
 
@@ -67,16 +104,16 @@ public class KmerGenerator {
 
     char[] mer = new char[K];
 
-    int nextRestart = (int) (R.nextGaussian()*25 + 200); // standard deviation 25, mean 200
+    int nextRestart = (int) (R.nextGaussian()*restartStdev + restartMean);
     int pos = K-1;
     for (int i = 0; i < numkmers; i++) {
       // check if we should "restart" the read
-      if (i >= nextRestart) {
+      if (i == nextRestart) {
         // generate first K-1 bases
         for (int j = 0; j < K - 1; j++)
           buf[j] = genChar(fix_gc);
         pos = K - 1;
-        nextRestart = i + (int) (R.nextGaussian()*25 + 200);
+        nextRestart += (int) (R.nextGaussian()*25 + 200);
       }
 
       // check if we wrapped around
@@ -97,7 +134,6 @@ public class KmerGenerator {
     buf = null;
 
     // write out the map
-
     pw.println("sampleid,kmer," + (normalize ? "norm_cnt" : "cnt"));
     final String sidcomma = sampleid+",";
     for (int i = 0; i < countMap.length; i++) {
@@ -125,5 +161,77 @@ public class KmerGenerator {
 //    System.out.println(" "+Arrays.toString(t));
     countMap[l]++;
   }
+
+
+  /**
+   * This version jumps around the sequence space
+   * @param pw
+   * @param sampleid
+   * @param numkmers
+   * @param normalize
+   * @param fix_gc
+   * @param restartStdev
+   * @param restartMean
+   */
+  private int[] generateKmerCounts_Restart(PrintWriter pw, String sampleid, int numkmers, int globalSpaceSize, boolean normalize, double fix_gc, double restartStdev, double restartMean) {
+    byte[] buf = new byte[globalSpaceSize];
+    Arrays.fill(buf, (byte)7);
+    int[] countMap = new int[1 << (K << 1)];
+
+    // generate first K-1 bases
+    for (int i = 0; i < K - 1; i++)
+      buf[i] = genChar(fix_gc);
+
+    char[] mer = new char[K];
+
+    int nextRestart = (int) (R.nextGaussian()*restartStdev + restartMean);
+    int pos = K-1;
+    for (int i = 0; i < numkmers; i++) {
+      // check if we should "restart" the read
+      if (i == nextRestart) {
+        // generate first K-1 bases
+        pos = R.nextInt(globalSpaceSize-K+1-250)+K-1;
+        for (int j = pos-K+1; j < pos; j++)
+          if (buf[j] == (byte)7)
+            buf[j] = genChar(fix_gc);
+        nextRestart += (int) (R.nextGaussian()*25 + 200);
+      }
+
+      // check if we wrapped around
+      if (pos == buf.length) {
+        // copy K-1 bases to front
+        System.arraycopy(buf, pos-K, buf, 0, K-1);
+        pos = K-1;
+      }
+
+      // generate next base
+      if (buf[pos] == (byte)7)
+        buf[pos] = genChar(fix_gc);
+
+      // count kmer
+      addKmer(buf, pos-K+1, countMap);
+
+      pos++;
+    }
+    buf = null;
+
+    // write out the map
+    pw.println("sampleid,kmer," + (normalize ? "norm_cnt" : "cnt"));
+    final String sidcomma = sampleid+",";
+    for (int i = 0; i < countMap.length; i++) {
+      if (countMap[i] != 0) {
+        pw.print(sidcomma);
+        decode(mer, i);
+        pw.print(mer);
+        pw.print(',');
+        if (normalize)
+          pw.println(((double)countMap[i])/numkmers);
+        else
+          pw.println(countMap[i]);
+      }
+    }
+    return countMap;
+  }
+
 
 }

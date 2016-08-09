583eeedc2bbbfae46352594798a47b42902681b3
Dylan Hutchison <dhutchis@cs.washington.edu>
2016-08-08 23:47:32 -0700

    Program to pipe kmers, optionally transforming them along the way

---
+++ src/main/java/edu/mit/ll/graphulo_ocean/OceanPipeKmers.java
@@ -0,0 +1,153 @@
+package edu.mit.ll.graphulo_ocean;
+
+import com.beust.jcommander.Parameter;
+import edu.mit.ll.graphulo.util.StatusLogger;
+import org.apache.hadoop.io.WritableComparator;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.SimpleLayout;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.Arrays;
+
+/**
+ * Ex: java -cp "/home/gridsan/dhutchison/gits/graphulo/target/graphulo-1.0.0-SNAPSHOT-all.jar" edu.mit.ll.graphulo_ocean.OceanPipeKmers -myriaHost node-109 -K 11 -numthreads 1 -lockDir "/home/gridsan/groups/istcdata/datasets/ocean_metagenome/csv_data/parsed_11_cnt_upload_claim" -outputDir "/home/gridsan/groups/istcdata/datasets/ocean_metagenome/csv_data/parsed_11_cnt_upload" < "/home/gridsan/groups/istcdata/datasets/ocean_metagenome/csv_data/parsed_11_cnt"
+ * Ex: java -cp "/home/gridsan/dhutchison/gits/graphulo/target/graphulo-1.0.0-SNAPSHOT-all.jar" edu.mit.ll.graphulo_ocean.OceanIngestKMers_csvtoMyria -myriaHost node-109 -inputDir "/home/gridsan/groups/istcdata/datasets/ocean_metagenome/csv_data/parsed_non_overlapped_11_cnt" -K 11 -numthreads 1 -lockDir "/home/gridsan/groups/istcdata/datasets/ocean_metagenome/csv_data/parsed_non_overlapped_11_cnt_upload_claim" -outputDir "/home/gridsan/groups/istcdata/datasets/ocean_metagenome/csv_data/parsed_non_overlapped_11_cnt_upload"
+ */
+public class OceanPipeKmers {
+  private static final Logger log = LogManager.getLogger(OceanPipeKmers.class);
+  private final StatusLogger slog = new StatusLogger();
+
+  public static void main(String[] args) {
+    LogManager.resetConfiguration();
+    log.removeAllAppenders();
+    log.addAppender(new ConsoleAppender(new SimpleLayout(), ConsoleAppender.SYSTEM_ERR));
+    log.setLevel(Level.INFO);
+    executeNew(args);
+  }
+
+  public static int executeNew(String[] args) { return new OceanPipeKmers().execute(args); }
+
+  private static class Opts extends Help {
+//    @Parameter(names = {"-listOfSamplesFile"}, required = true)
+//    public String listOfSamplesFile;
+
+//    @Parameter(names = {"-everyXLines"})
+//    public int everyXLines = 1;
+
+//    @Parameter(names = {"-startOffset"})
+//    public int startOffset = 0;
+
+    @Parameter(names = {"-K"}, required = true)
+    public int K;
+
+    @Parameter(names = {"-rc"}, description = "reverseComplement")
+    public boolean rc = false;
+
+    @Parameter(names = {"-lex"}, description = "lesserLex; overrides -rc")
+    public boolean lex = false;
+
+    @Parameter(names = {"-binary"})
+    public boolean binary = false;
+
+    @Override
+    public String toString() {
+      return "Opts{" +
+          "K=" + K +
+          ", rc=" + rc +
+          ", lex=" + lex +
+          ", binary=" + binary +
+          '}';
+    }
+  }
+
+  enum ReadMode { FORWARD, RC, LEX }
+
+  /** @return Number of lines processed */
+  public int execute(final String[] args) {
+    final Opts opts = new Opts();
+    opts.parseArgs(OceanPipeKmers.class.getName(), args);
+    log.info(OceanPipeKmers.class.getName() + " " + opts);
+    final ReadMode readMode;
+    if (opts.lex)
+      readMode = ReadMode.LEX;
+    else if (opts.rc)
+      readMode = ReadMode.RC;
+    else
+      readMode = ReadMode.FORWARD;
+    final GenomicEncoder G = new GenomicEncoder(opts.K);
+
+    int linesProcessed = 0;
+    try (
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        PrintStream writer = System.out
+    ) {
+      String line;
+//      final DataOutputStream dos = new DataOutputStream(writer);
+//      int commaPos = -1;
+
+      while ((line = reader.readLine()) != null) {
+
+        switch (readMode) {
+          case FORWARD:
+            writer.println(line);
+            break;
+          default:
+            //parseAndWrite(readMode, line, writer, commaPos)
+            char[] linearr = line.toCharArray();
+//            commaPos = findComma(linearr, commaPos);
+//            if (commaPos == -1)
+//              commaPos = findComma(linearr, -1);
+//            if (commaPos == -1) {
+//              log.warn("bad line: "+line);
+//              continue;
+//            }
+
+            byte[] enc = G.encode(linearr);
+            char[] dec;
+            switch (readMode) {
+              case RC:
+//                System.err.print(linearr);
+//                System.err.print(" "+linearr.length+" "+Arrays.toString(Arrays.copyOf(linearr, 11))+" "+Arrays.toString(G.decode(enc))+" ");
+                G.reverseComplement(enc);
+                dec = G.decode(enc);
+//                System.err.print(dec.length+" "+Arrays.toString(dec)+" ");
+                System.arraycopy(dec, 0, linearr, 0, dec.length);
+                break;
+              case LEX:
+                byte[] encrc = Arrays.copyOf(enc, enc.length);
+                G.reverseComplement(encrc);
+                dec = G.decode(WritableComparator.compareBytes(enc, 0, enc.length, encrc, 0, encrc.length) <= 0
+                    ? enc : encrc);
+                System.arraycopy(dec, 0, linearr, 0, dec.length);
+                break;
+              default: throw new AssertionError();
+            }
+//            System.err.println(linearr);
+            writer.println(linearr);
+        }
+        linesProcessed++;
+        if (linesProcessed % 1_000 == 0)
+          slog.logPeriodic(log, "Processed lines: "+linesProcessed);
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    System.err.flush();
+    return linesProcessed;
+  }
+
+  private static int findComma(char[] arr, int start) {
+    for (int i = start == -1 ? 0 : start; i < arr.length; i++)
+      if (arr[i] == ',')
+        return i;
+    return -1;
+  }
+
+}
